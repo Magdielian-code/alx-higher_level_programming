70a244188dbbe2b7894b61680c3d2525363dd0b1
Magdielian-code <oziomaagaecheta81@gmail.com>
2023-11-28 10:35:42 -0800

    updated 5- print file.

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for num in range(0, 100):
-    print("{:02d}, ".format(num), end="")
-print("99")
+for num in range(0, 99):
+    print('{:02d}, '.format(num), end='')
+print('99')
