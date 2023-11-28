be11aedd01baedb702fde0c229c7bc6869260fe6
Magdielian-code <oziomaagaecheta81@gmail.com>
2023-11-28 09:35:43 -0800

    updated the last digit file usinf format() instead of f-strings.

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -7,8 +7,8 @@ if number < 0:
 else:
     lastdigit = number % 10
 if lastdigit > 5:
-    print(f"Last digit of {number} is {lastdigit} and is greater than 5")
+    print("Last digit of {:d} is {:d} and is greater than 5".format(number, lastdigit))
 elif lastdigit < 6 and lastdigit != 0:
-    print(f"Last digit of {number} is {lastdigit} and is greater than 5")
+    print("Last digit of {:d} is {:d} and is less than 6 and not 0".format(number, lastdigit))
 else:
-    print(f"Last digit of {number} is 0 and is 0")
+    print("Last digit of {:d} is 0 and is 0".format(number))
