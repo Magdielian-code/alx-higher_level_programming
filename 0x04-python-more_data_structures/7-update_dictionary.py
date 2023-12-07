10e73d616d4d2abed15d49fae76fcef8b81d1975
Magdielian-code <oziomaagaecheta81@gmail.com>
2023-12-06 17:21:07 -0800

    added file 7

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    if key in a_dictionary[key]:
-        a_dictionary[key]= value
-    else:
-        a_dictionary[key]=value
+    a_dictionary[key] = value
     return a_dictionary
