c20de6a081923cfa58c43efdf38811e4b2b312f5
Magdielian-code <oziomaagaecheta81@gmail.com>
2023-12-20 03:11:34 -0800

    fixed the f-string issue

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -6,5 +6,5 @@ def safe_print_division(a, b):
     except (TypeError, ZeroDivisionError):
         div = None
     finally:
-        print("Inside result: {:d}".format(div))
-    return div
+        print("Inside result: {}".format(div))
+    return (div)
