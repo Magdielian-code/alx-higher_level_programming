5143fd653936bbf5c8471269c935eafc10efde7c
oziomaagaecheta81@gmail.com <oziomaagaecheta81@gmail.com>
2024-01-09 16:33:39 -0800

    added write_file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Write a function that writes a string to a text file (UTF8) and returns the number of characters written:"""
+
+def write_file(filename="", text=""):
+    """writes a string to file"""
+    with open(filename, "w", encoding="utf-8") as f:
+        return f.write(text)
