157aebe69f0fda141ccb1e888492d579d5c5043f
oziomaagaecheta81@gmail.com <oziomaagaecheta81@gmail.com>
2024-01-09 16:16:27 -0800

    updated file 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """This module defines a text file-reading function"""
 
-def read_file(filename=""):
+def read_file(filename="", text=""):
     """Prints the contents of the text file"""
     with open(filename, encoding="utf-8") as f:
         print(f.read(), end="")
