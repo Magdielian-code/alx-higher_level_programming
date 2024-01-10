595d59cbf0b35649db59f0307cef63130e31b239
oziomaagaecheta81@gmail.com <oziomaagaecheta81@gmail.com>
2024-01-09 16:51:23 -0800

    added json_string file

---
+++ 0x0B-python-input_output/3-json_string.py
@@ -0,0 +1,7 @@
+#1/usr/bin/python3
+"""Write a function that returns the JSON representation of an object (string):"""
+import json
+
+def to_json_string(my_obj):
+    """Returns the json copy of a string object."""
+    return json.dumps(my_obj)
