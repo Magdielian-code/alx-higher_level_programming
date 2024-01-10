2513e557aec28da51b5315573e851c3a7019729c
oziomaagaecheta81@gmail.com <oziomaagaecheta81@gmail.com>
2024-01-09 17:01:02 -0800

    added file 4

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Write a function that returns an object (Python data structure) represented by a JSON string:"""
+import json
+
+def from_json_string(my_str):
+    """Returns a python object from a json string"""
+    return json.loads(my_obj)
